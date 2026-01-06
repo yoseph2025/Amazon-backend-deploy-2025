1a014d43390f03edfb69d131740666026ef0f508
yoseph2025 <yosephgashaye1@gmail.com>
2026-01-06 21:35:33 +0300

    new

--- amazon-clon-2025/vite.config.js
+++ amazon-clon-2025/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Amazon-backend-deploy-2025/",
   plugins: [react()],
-})
+});
